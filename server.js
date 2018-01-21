a129c5c83dd1eeaecb538f6639cc982cabe8829a
Scott Mangiapane <scottmangiapane@gmail.com>
2018-01-21 15:56:30 +0000

    Switched to pug, changed logo, added RAM usage bar

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const ip = require('ip');
+const os = require('os');
 const path = require('path');
 
 const app = express();
@@ -7,10 +8,13 @@ const app = express();
 app.use(express.static(__dirname + '/static'))
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index', {ip: ip.address()});
+	const free = os.freemem();
+	const total = os.totalmem();
+	const memory = Math.round(100 * (total - free) / total);
+	res.render('index', {ip: ip.address(), memory: memory});
 });
 
 app.listen(8080, () => console.log('Server running on port 8080.'));
