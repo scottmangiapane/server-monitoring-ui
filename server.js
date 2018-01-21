eb800f016329b5200137a69d5d5875c541771e32
Scott Mangiapane <scottmangiapane@gmail.com>
2018-01-21 04:51:38 +0000

    Added simple jade website to dynamically show ip

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const ip = require('ip');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello world."));
+app.use(express.static(__dirname + '/static'))
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => {
+	res.render('index', {ip: ip.address()});
+});
 
 app.listen(8080, () => console.log('Server running on port 8080.'));
