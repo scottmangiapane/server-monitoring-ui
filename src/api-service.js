a424dfcc54be0fcfb5ba23cb8820fdcd387ce7b8
Scott Mangiapane <scottmangiapane@gmail.com>
2020-07-12 16:39:49 -0700

    Made info data dynamic

--- src/api-service.js
+++ src/api-service.js
@@ -1,1 +1,10 @@
-export default {};
+import axios from 'axios';
+
+const base = process.env.VUE_APP_HTTP_API;
+
+export default {
+    getInfo(payload) {
+        const url = `${ base }/info`;
+        return axios.get(url, payload).then(res => res.data);
+    }
+};
