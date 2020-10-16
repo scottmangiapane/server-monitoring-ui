98faa135b0bece3473b3ae271fe744df1fa1f9f6
Scott Mangiapane <scottmangiapane@gmail.com>
2020-10-15 18:59:28 -0700

    Humanize uptime for readability

--- src/views/dashboard/time.vue
+++ src/views/dashboard/time.vue
@@ -3,16 +3,18 @@
         <p class="text-dark">Time</p>
         <div class="spacer"></div>
         <div class="bordered">
-            <p class="text-small">Uptime: {{ time.uptime }} seconds</p>
+            <p class="text-small">Uptime: {{ uptime }}</p>
         </div>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import humanize from 'humanize-duration';
+
 export default {
     computed: mapState({
-        time: state => state.time
+        uptime: state => humanize(state.time.uptime * 1000, { largest: 2 })
     })
 };
 </script>
